d495c10adb2a64b4c89c6c56ae1401f4c1d460df
Bangia <nitinbangia8@gmail.com>
2026-09-23 18:10:43 +0530

    fix: handle category on product details

--- src/pages/product-details/index.jsx
+++ src/pages/product-details/index.jsx
@@ -23,7 +23,10 @@ const ProductDetailsPage = ({ query }) => {
   if (!isLoading && !isError && product) {
     content = (
       <>
-        <ProductDetailsBreadcrumb category={product.category.name} title={product.title} />
+        <ProductDetailsBreadcrumb
+          category={product.category?.name || product.category || "Products"}
+          title={product.title}
+        />
         <ProductDetailsArea productItem={product} />
       </>
     );
